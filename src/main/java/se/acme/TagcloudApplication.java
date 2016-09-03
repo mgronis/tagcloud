94b6898b95ca48250296ad66e350ea5349e3eef9
mgronis <m.gronis@gmail.com>
2016-09-03 17:06:58 +0200

    Added starting point for spring-boot

---
+++ src/main/java/se/acme/TagcloudApplication.java
@@ -0,0 +1,12 @@
+package se.acme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TagcloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TagcloudApplication.class, args);
+	}
+}
---
+++ src/test/java/se/acme/TagcloudApplicationTests.java
@@ -0,0 +1,16 @@
+package se.acme;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TagcloudApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
