753dd090e175972888ff32df164f07babec6ac48
mgronis <m.gronis@gmail.com>
2016-09-04 17:37:20 +0200

    Added prototype of a UI

--- src/main/java/se/acme/controller/FakeRestController.java
+++ src/main/java/se/acme/controller/FakeRestController.java
@@ -4,15 +4,16 @@ import com.google.common.collect.ImmutableList;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import se.acme.transport.TagEntry;
-import se.acme.transport.TestTO;
+
+import java.util.Collection;
 
 @RestController
 @RequestMapping("/fake/")
 public class FakeRestController {
 
-    @RequestMapping("data/")
-    public TestTO fakeData() {
-        return new TestTO(ImmutableList.of(new TagEntry(1, "Volvo"), new TagEntry(2, "Opel"), new TagEntry(3, "Saab")));
+    @RequestMapping("data")
+    public Collection<TagEntry> fakeData() {
+        return ImmutableList.of(new TagEntry(1, "Volvo"), new TagEntry(2, "Opel"), new TagEntry(3, "Saab"));
     }
 
 }
