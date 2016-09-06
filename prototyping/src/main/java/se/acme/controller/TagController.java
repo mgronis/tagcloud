9b591d5c93c1787a9533823ab5c48c8d8ab76937
mgronis <m.gronis@gmail.com>
2016-09-06 19:37:30 +0200

    Added more test implementations to the prototyping project

--- prototyping/src/main/java/se/acme/controller/TagController.java
+++ prototyping/src/main/java/se/acme/controller/TagController.java
@@ -4,14 +4,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-//@RestController
 @Controller
 public class TagController {
 
-//    @RequestMapping("/")
-    @RequestMapping(value="/",method = RequestMethod.GET)
+    @RequestMapping(path="/ang",method = RequestMethod.GET)
     public String index() {
-        return "Greetings from Spring Boot!";
+        return "redirect:views/angular.html";
     }
 
 }
